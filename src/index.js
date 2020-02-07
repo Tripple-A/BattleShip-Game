ca2bc1e29a9f0a1c2b1466ded6d669d6932ac058
Tripple-A <biodun9@gmail.com>
2020-02-07 18:51:19 +0100

    added module for DOM manipulation and player and computer boards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import Ship from './factories/ships';
 import Gameboard from './factories/gameboard';
+import Battle from './DOM/DOM';
 import { Player, Computer } from './factories/players';
+import './style.css';
 
+const gridContainer = document.getElementById('gridContainer');
+Battle.createGrids(10, gridContainer);
+const shipMaker = document.querySelector('.shipMaker');
+Battle.createShips(shipMaker);
+const placer = (i) => {
+console.log(i);
+};
